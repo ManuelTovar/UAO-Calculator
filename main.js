fa2517190dd54e1f6b20aecb7aee8153be88280d
Isabella <isabella.moreno@uao.edu.co>
2020-02-24 17:07:00 -0500

    Se corrijio funcion de isNumberKey y lenguaje eng

--- main.js
+++ main.js
@@ -6,30 +6,30 @@ function Operations() {
   var numNumber1 = document.getElementById("nuNumber1").value;
   var numNumber2 = document.getElementById("nuNumber2").value;
 
-  if (operation == "Suma") {
+  if (operation == "Sum") {
     console.log("Entro a suma");
     var nuResult = parseFloat(numNumber1) + parseFloat(numNumber2);
-  } else if (operation == "Resta") {
+  } else if (operation == "Substraction") {
     console.log("Entro a resta");
     var nuResult = parseFloat(numNumber1) - parseFloat(numNumber2);
-  } else if (operation == "Multiplicación") {
+  } else if (operation == "Multiplication") {
     console.log("Entro a multiplicacion");
     var nuResult = parseFloat(numNumber1) * parseFloat(numNumber2);
   } else if (operation == "Division") {
     console.log("Entro a division");
     var nuResult = parseFloat(numNumber1) / parseFloat(numNumber2);
-  }
+  } 
 
-  console.log(nuResult);
+  console.log(nuResult.toFixed(2));
 
-  document.getElementById("result").innerHTML = nuResult;
+  document.getElementById("result").innerHTML = nuResult.toFixed(2);
 
 }
 
 function isNumberKey(evt) {
   var charCode = (evt.which) ? evt.which : event.keyCode
-  if (charCode > 31 && (charCode < 48 || charCode > 57))
-    return false;
+  if ((charCode >= 48 && charCode <= 57 || charCode == 46 || charCode == 44))
+    return true;
 
-  return true;
+  return false;
 }
